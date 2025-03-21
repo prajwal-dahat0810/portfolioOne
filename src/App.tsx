0faac9b57b66610b5da218044c300acf0b120a1c
prajwal-dahat0810 <dahatprajwal11@gmail.com>
2025-03-21 08:18:37 +0530

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Landing />} path="/" />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Analytics />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Landing />} path="/" />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
